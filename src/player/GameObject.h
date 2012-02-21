31bb3c74ccaa9783df37572a66ffd61bed0ca467
Roger F <oced.roger@gmail.com>
2012-02-20 18:03:22 -0800

    added (int) spriteID data member and (Point) pos

--- src/player/GameObject.h
+++ src/player/GameObject.h
@@ -1,16 +1,15 @@
 #ifndef GAMEOBJECT_H_
 #define GAMEOBJECT_H_
 
-#include "Sprite.h"
-#include "GOPos.h"
+#include "Point.h"
 
 // Abstract Base Class
 class GameObject
 {
 	protected:
 		int GObjID;
-		GOPos pos;
-		Sprite sprite;
+		int spriteID;
+		Point pos;
 	public:
 		virtual GOPos Position() = 0;
 };

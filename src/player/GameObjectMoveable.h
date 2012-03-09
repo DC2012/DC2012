9d031f1277212a74441b7996211fb0e9f13d7d9a
Roger F <oced.roger@gmail.com>
2012-03-08 19:00:14 -0800

    commented out pure virtual functions

--- src/player/GameObjectMoveable.h
+++ src/player/GameObjectMoveable.h
@@ -2,13 +2,11 @@
 #define GAMEOBJECTMOVEABLE_H_
 
 #include "GameObject.h"
-//#include "Direction.h"
 
 class GameObjectMoveable : public GameObject
 {
 	protected:
 		int playerID_;
-		//Direction dir_;
 		int speed_;
 	public:
 		explicit GameObjectMoveable(int objID,
@@ -18,8 +16,9 @@ class GameObjectMoveable : public GameObject
 									int playerID,
 									int speed)
 		: GameObject(objID, degree, posX, posY), playerID_(playerID), speed_(speed){}
-		virtual void accelerate(int amount) = 0;
-		virtual void decelerate(int amount) = 0;
+		
+		// virtual void accelerate(int amount) = 0;
+		// virtual void decelerate(int amount) = 0;
 };
 
 #endif

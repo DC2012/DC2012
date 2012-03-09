96b32e2b3b3ff0af0c3987ef1767be0444c410bc
Roger F <oced.roger@gmail.com>
2012-03-09 07:25:55 -0800

    fixed tabbing

--- src/player/GOM_Ship.h
+++ src/player/GOM_Ship.h
@@ -8,20 +8,20 @@ class GOM_Ship : public GameObjectMoveable
 	private:
 		int health_;
 		int attackPower_;
-        
+		
 	public:
-        //constructor
+		//constructor
 		explicit GOM_Ship(int GObjID = 0, int degree = 0, int posX = 0, int posY = 0, 
 			int playerID = 0, int speed = 0, int health = 0, int attackPower = 0);
 		// destructor
 		virtual ~GOM_Projectile(){}
-        
-        // inherited from GameObjectMoveable
+
+		// inherited from GameObjectMoveable
 		virtual void accelerate(int amount);
 		virtual void decelerate(int amount);
-        
-        //for testing
-        virtual void print(std::ostream& os) const;
+
+		//for testing
+		virtual void print(std::ostream& os) const;
 };
 
 #endif

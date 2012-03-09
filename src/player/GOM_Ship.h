7d098bd4f42f20c87894bfcad498696623d45233
Roger F <oced.roger@gmail.com>
2012-03-09 07:23:24 -0800

    merged from zach

--- src/player/GOM_Ship.h
+++ src/player/GOM_Ship.h
@@ -5,6 +5,23 @@
 
 class GOM_Ship : public GameObjectMoveable
 {
+	private:
+		int health_;
+		int attackPower_;
+        
+	public:
+        //constructor
+		explicit GOM_Ship(int GObjID = 0, int degree = 0, int posX = 0, int posY = 0, 
+			int playerID = 0, int speed = 0, int health = 0, int attackPower = 0);
+		// destructor
+		virtual ~GOM_Projectile(){}
+        
+        // inherited from GameObjectMoveable
+		virtual void accelerate(int amount);
+		virtual void decelerate(int amount);
+        
+        //for testing
+        virtual void print(std::ostream& os) const;
 };
 
 #endif
---
+++ src/player/GameObjectFactory.h
@@ -0,0 +1,49 @@
+#ifndef GAMEOBJECTMOVEABLE_H_
+#define GAMEOBJECTMOVEABLE_H_
+
+#include "GameObjectMoveable.h"
+#include "GameObjectStationary.h"
+#include "GameObject.h"
+
+class GameObjectFactory
+{
+	public:
+		//empty constructor, takes in a istream
+		GameObjectFactory(std::istream& is): in_(&is) {}
+		//call to create the new object
+		GameObject *create()
+		{
+			std::string type;
+			int objID, degree, posX, posY, playerID, speed, health, ttl, damage, attackPower;
+			if (!(*in_ >> type))
+			  return 0;
+			if (type == "P")
+			{
+				*in_ >> objID >> degree >> posX >> playerID >> speed >> ttl >> damage;
+				return new GOM_Projectile(objId, degree, posX, posY, playerID, speed, ttl, damage);
+			}
+			  
+			if (type == "S")
+			{
+				*in_ >> objID >> degree >> posX >> playerID >> speed >> health >> attackPower;
+				return new GOM_Ship(objId, degree, posX, posY, playerID, speed, health, attackPower);
+			}
+			  
+			if (type == "O")
+			{
+				return new GOS_Obstacle(*in_);
+			}
+			  
+			if (type == "B")
+			{
+				return new GOS_Powerup(*in_);
+			}
+			
+			return 0;  // if it's not one of the valid types
+		}
+	private:
+		std::istream  *in_;  
+
+};
+
+#endif

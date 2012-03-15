06e0dd7b72c84ecc84a68d2a453a527e473ca100
Clarence Chuahuico <cchuahuico@gmail.com>
2012-03-14 23:01:59 -0700

    Updated game window to include timer and other stuff

--- src/graphics/gamewindow.cpp
+++ src/graphics/gamewindow.cpp
@@ -3,15 +3,40 @@
 #include <QGraphicsPixmapItem>
 #include <QKeyEvent>
 #include <QMessageBox>
+#include <QCursor>
 
-GameWindow::GameWindow(QWidget *parent) :
-    QGraphicsView(parent)
+GameWindow::GameWindow(QWidget *parent)
+    : QGraphicsView(parent), timer_(this)
 {
     setRenderHints(QPainter::Antialiasing | QPainter::SmoothPixmapTransform);
+    setCursor(QCursor(QPixmap("../../sprites/spriteCursor.png")));
+    setFixedSize(CLIENT_WIDTH, CLIENT_HEIGHT);
+    setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 
     scene_ = new QGraphicsScene();
-    scene_->setSceneRect(0, 0, 700, 500);
+    scene_->setSceneRect(0, 0, CLIENT_WIDTH, CLIENT_HEIGHT);
     setScene(scene_);
+
+    // load map - the positions will eventually be stored in an xml file
+    QPixmap land("../../sprites/tileLand1.png");
+    QPixmap sea("../../sprites/tileWater1.png");
+    for (int x = 0; x < CLIENT_WIDTH; x += 50)
+    {
+        for(int y = 0; y < CLIENT_HEIGHT / 3; y += 50)
+        {
+            QGraphicsPixmapItem *landTile = new QGraphicsPixmapItem(land);
+            landTile->setOffset(x, y);
+            scene_->addItem(landTile);
+        }
+
+        for(int y = CLIENT_HEIGHT / 3; y < CLIENT_HEIGHT; y += 50)
+        {
+            QGraphicsPixmapItem *seaTile = new QGraphicsPixmapItem(sea);
+            seaTile->setOffset(x, y);
+            scene_->addItem(seaTile);
+        }
+    }
 
     // these pixmap objects will be replaced by proper
     // game objects once the implementation is complete
@@ -19,6 +44,15 @@ GameWindow::GameWindow(QWidget *parent) :
     ship_ = new QGraphicsPixmapItem(ship);
     ship_->setPos(20, 20);
     scene_->addItem(ship_);
+
+    connect(&timer_, SIGNAL(timeout()), this, SLOT(updateGame()));
+
+    this->show();
+}
+
+void GameWindow::start()
+{
+    timer_.start(1000 / FRAME_RATE);
 }
 
 void GameWindow::keyPressEvent(QKeyEvent *event)
@@ -26,19 +60,28 @@ void GameWindow::keyPressEvent(QKeyEvent *event)
     switch(event->key())
     {
     case Qt::Key_W:
+    case Qt::Key_Up:
         ship_->setOffset(ship_->offset().x(), ship_->offset().y() - 1);
         break;
     case Qt::Key_A:
+    case Qt::Key_Left:
         ship_->setOffset(ship_->offset().x() - 1, ship_->offset().y());
         break;
     case Qt::Key_S:
+    case Qt::Key_Down:
         ship_->setOffset(ship_->offset().x(), ship_->offset().y() + 1);
         break;
     case Qt::Key_D:
+    case Qt::Key_Right:
         ship_->setOffset(ship_->offset().x() + 1, ship_->offset().y());
         break;
     case Qt::Key_Space:
         QMessageBox::information(this, "Fire!", "Assume that a bullet was fired.");
         break;
     }
+}
+
+void GameWindow::updateGame()
+{
+
 }
--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ int main(int argc, char **argv)
     GameWindow *gameWindow = new GameWindow();
 
     gameWindow->setFocus();
-    gameWindow->show();
+    gameWindow->start();
 
     return app.exec();
 }

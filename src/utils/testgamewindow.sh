e24c491ce6dd2d1f5be5efd290da2c652355579b
Clarence Chuahuico <cchuahuico@gmail.com>
2012-03-14 23:24:50 -0700

    Modified test script to update sprite location in cpp file

--- src/utils/testgamewindow.sh
+++ src/utils/testgamewindow.sh
@@ -9,9 +9,16 @@ fi
 
 rm -rf ~/GameTest
 mkdir -p ~/GameTest/graphics
+mkdir -p ~/GameTest/graphics/sprites
 cp ../graphics/gamewindow.* ~/GameTest/graphics
 cp ../main.cpp ~/GameTest
+cp ../../sprites/tileLand1.png ~/GameTest/graphics/sprites
+cp ../../sprites/tileWater1.png ~/GameTest/graphics/sprites
+cp ../../sprites/spriteShip1.png ~/GameTest/graphics/sprites
+cp ../../sprites/spriteCursor.png ~/GameTest/graphics/sprites
 cd ~/GameTest
+sed -ie 's/\.\.\/\.\.\//graphics\//g' graphics/gamewindow.cpp 
 $QM -project
 $QM
 make
+./GameTest

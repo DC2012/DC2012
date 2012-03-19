a3c034ac83484fdfcdf433bc57297754d8a523ac
Kevin Han <kevin9han@yahoo.ca>
2012-03-19 11:33:15 -0700

    added ship size

---
+++ sprites/sprites.h
@@ -0,0 +1,29 @@
+#define SPRITE_SHIP1 QString("../../sprites/spriteShip1.png")
+#define SPRITE_SHIP1_HIT QString("../../sprites/spriteShip1Hit.png")
+#define SPRITE_SHIP1_MOVE1 QString("spriteShipMove1.png")
+#define SPRITE_SHIP1_MOVE2 QString("spriteShipMove2.png")
+#define SPRITE_SHIP1_MOVE3 QString("spriteShipMove3.png")
+#define SHIP1_WIDTH 20
+#define SHIP1_HEIGHT 90
+
+#define SPRITE_SHIP2 QString("spriteShip2.png")
+#define SPRITE_SHIP2_HIT QString("spriteShip2Hit.png")
+#define SPRITE_SHIP2_MOVE1 QString("spriteShip2Move1.png")
+#define SPRITE_SHIP2_MOVE2 QString("spriteShip2Move2.png")
+#define SPRITE_SHIP2_MOVE3 QString("spriteShip2Move3.png")
+#define SHIP2_WIDTH 30
+#define SHIP2_HEIGHT 125
+
+#define SPRITE_EXPLOSION1 QString("spriteExplosion1.png")
+#define SPRITE_EXPLOSION2 QString("spriteExplosion2.png")
+#define SPRITE_EXPLOSION3 QString("spriteExplosion3.png")
+#define SPRITE_EXPLOSION4 QString("spriteExplosion4.png")
+#define SPRITE_EXPLOSION5 QString("spriteExplosion5.png")
+#define SPRITE_EXPLOSION6 QString("spriteExplosion6.png")
+
+#define SPRITE_CANNONBALL QString("spriteCannonball.png")
+#define SPRITE_BULLET QString("spriteBullet.png")
+#define SPRITE_CURSOR QString("spriteCursor.png")
+#define TILE_WATER1 QString("tileWater1.png")
+#define TILE_WATER2 QString("tileWater2.png")
+#define TILE_LAND1 QString("tileLand1.png")

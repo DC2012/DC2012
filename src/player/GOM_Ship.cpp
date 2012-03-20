9669f0842d5d5249a1388fd8330d42402463a1dd
Roger F <oced.roger@gmail.com>
2012-03-19 22:40:20 -0700

    removed some testing codes

--- src/player/GOM_Ship.cpp
+++ src/player/GOM_Ship.cpp
@@ -41,12 +41,12 @@ void GOM_Ship::move()
 {
 	Point pt;
 	
-	if(actionFlags_[ROTATE_L] && speed_ > 0.5) // rotation not allowed when ship is moving too slowly
+	if(actionFlags_[ROTATE_L])
 	{
 		degree_.rotate(-1);
 	}
 	
-	if(actionFlags_[ROTATE_R] && speed_ > 0.5)
+	if(actionFlags_[ROTATE_R])
 	{
 		degree_.rotate(1);
 	}

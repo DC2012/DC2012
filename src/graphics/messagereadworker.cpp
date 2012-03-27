d3b1eb8d4647d9f4bf6ea97b8f67d8cc7cb4a941
Clarence Chuahuico <cchuahuico@gmail.com>
2012-03-26 18:16:24 -0700

    Removed testing code

--- src/graphics/messagereadworker.cpp
+++ src/graphics/messagereadworker.cpp
@@ -12,11 +12,6 @@ void MessageReadWorker::readMessages()
     Client* client = Client::getInstance();
     Message *msg;
 
-    Message mes(1);
-    mes.setData("hello");
-    mes.setAll("hello", Message::CREATION);
-    emit messageReceived(&mes);
-
     while((msg = client->read()))
         emit messageReceived(msg);
 }

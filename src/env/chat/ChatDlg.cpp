5ccccf72a7826aaa2b371dd546ef3c338c9a3feb
Tyler I <tyler@dynamicshark.com>
2012-03-28 10:50:33 -0700

    ChatDlg now appears bottom-left corner of screen and is transparent

--- src/env/chat/ChatDlg.cpp
+++ src/env/chat/ChatDlg.cpp
@@ -1,14 +1,18 @@
 #include "ChatDlg.h"
 #include "ui_ChatDlg.h"
 #include <QMessageBox>
+#include "../../../src/graphics/gamewindow.h"
+#include <QPalette>
 
 ChatDlg::ChatDlg(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::ChatDlg)
 {
     ui->setupUi(this);
+    connect(ui->lineEdit_input, SIGNAL(returnPressed()), this, SLOT(doChat()));
 
-    connect(ui->lineEdit, SIGNAL(returnPressed()), this, SLOT(doChat()));
+    this->setWindowOpacity(0.8);
+    this->setWindowFlags(Qt::FramelessWindowHint);
 }
 
 ChatDlg::~ChatDlg()
@@ -18,10 +22,11 @@ ChatDlg::~ChatDlg()
 
 void ChatDlg::doChat()
 {
-    QMessageBox mb;
-    mb.setText("Stuff!");
-    mb.exec();
 
+    // This is where we will need to send a Message to server of
+    // type CHAT
+    GameWindow *p = (GameWindow *) this->parent();
+    p->setChatting(false);
     this->close();
 
 }
--- src/graphics/gamewindow.cpp
+++ src/graphics/gamewindow.cpp
@@ -4,6 +4,7 @@
 
 #include <QGraphicsPixmapItem>
 #include <QKeyEvent>
+#include <QLineEdit>
 #include <QCursor>
 #include "../../src/env/chat/ChatDlg.h"
 #include <QMessageBox>
@@ -19,9 +20,12 @@ GameWindow::GameWindow(QWidget *parent)
     setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 
     // env chat message stuff
-    chatdlg_ = new ChatDlg;
+    chatdlg_ = new ChatDlg(this);
+
     chatdlg_->setModal(false);
+    chatdlg_->setVisible(false);
     isChatting_ = false;
+    chatdlg_->setGeometry(0, (this->geometry().height() - 150), 400, 150);
 
     scene_ = new QGraphicsScene();
     scene_->setSceneRect(0, 0, CLIENT_WIDTH, CLIENT_HEIGHT);
@@ -76,6 +80,8 @@ void GameWindow::start()
 
 void GameWindow::keyPressEvent(QKeyEvent *event)
 {
+    QLineEdit *le = chatdlg_->findChild<QLineEdit *>("lineEdit_input");
+
     switch(event->key())
     {
     case Qt::Key_W:
@@ -100,11 +106,13 @@ void GameWindow::keyPressEvent(QKeyEvent *event)
         QMessageBox::information(this, "Fire!", "Assume that a bullet was fired.");
         break;
     case Qt::Key_Return:
+    case Qt::Key_Enter:
         // this will change when we have the chat portion as it's own
-        // frame/widget.
-        if(this->isChatting())
+        // frame/widget. We also set the lineEdit_input to have focus()
+        if(!this->isChatting() || !chatdlg_->isVisible())
         {
             chatdlg_->show();
+            le->setFocus();
             this->setChatting(true);
         }
         else

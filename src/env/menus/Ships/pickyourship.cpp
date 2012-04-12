584bfca99a78d0db2d01a97be0de292854a8e8cd
Tyler <hypino@Hypino.(none)>
2012-04-11 22:01:08 -0700

    merging

--- src/env/menus/Ships/pickyourship.cpp
+++ src/env/menus/Ships/pickyourship.cpp
@@ -29,7 +29,7 @@ void PickYourShip::on_btnShip1_clicked()
 {
     Message msg;
     msg.setType(Message::RESPAWN);
-    msg.setData("1");
+    msg.setData("0");
     msg.setID(0);
 
     client_->write(&msg);
@@ -40,7 +40,7 @@ void PickYourShip::on_btnShip2_clicked()
 {
     Message msg;
     msg.setType(Message::RESPAWN);
-    msg.setData("2");
+    msg.setData("1");
     msg.setID(0);
 
     client_->write(&msg);
@@ -51,7 +51,7 @@ void PickYourShip::on_btnShip3_clicked()
 {
     Message msg;
     msg.setType(Message::RESPAWN);
-    msg.setData("3");
+    msg.setData("2");
     msg.setID(0);
 
     client_->write(&msg);

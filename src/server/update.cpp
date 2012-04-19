24188db27c394ef8bae4031d8b50ae4332158714
Roger-F- <oced.roger@gmail.com>
2012-04-19 13:37:13 -0700

    fixed null iterator increment after a map element is erased

--- src/server/update.cpp
+++ src/server/update.cpp
@@ -6,6 +6,7 @@ void update(sigval arg)
     Server *server;
     Message sendMsg;
     std::map<int, GameObject*>::iterator ii, iii, itShip, iitShip;
+    std::map<int, GameObject*>::iterator ii_copy, itShip_copy;
     std::ostringstream ostr;
     GOM_Projectile *projectile;
     GOM_Ship *ship, *bship;
@@ -16,19 +17,22 @@ void update(sigval arg)
     server = pdata->server;
     
     // move the projectiles and do hit detection
-    for(ii = pdata->projectiles.begin(); ii != pdata->projectiles.end(); ++ii)
+    for(ii = pdata->projectiles.begin(); ii != pdata->projectiles.end();)
     {
+        ii_copy = ii;
         projectile = (GOM_Projectile *)(ii->second);
 
         //  move() returns true if ttl_ is 0
         if(projectile->move())
         {
             //  remove the projectile from map is true
-            delete pdata->projectiles[ii->first];
-            pdata->projectiles.erase(ii->first);
+            ++ii;
+            delete pdata->projectiles[ii_copy->first];
+            pdata->projectiles.erase(ii_copy->first);
         }
         else
         {
+            ++ii;
             // check collision with ships
             for(iii = pdata->ships.begin(); iii != pdata->ships.end(); ++iii)
             {
@@ -52,8 +56,8 @@ void update(sigval arg)
                     std::cout << "sent deletion: " << sendMsg.getData() << "\n";
 
                     // delete the projectile
-                    delete pdata->projectiles[ii->first];
-                    pdata->projectiles.erase(ii->first);
+                    delete pdata->projectiles[ii_copy->first];
+                    pdata->projectiles.erase(ii_copy->first);
 
                     break;
                 }
@@ -62,46 +66,49 @@ void update(sigval arg)
     } // end of for
 
     // do hit detection on all ships against all other ships
-    for(itShip = pdata->ships.begin(); itShip != pdata->ships.end(); ++itShip)
+    for(itShip = pdata->ships.begin(); itShip != pdata->ships.end();)
     {
+        itShip_copy = itShip;
         ship = (GOM_Ship *)(itShip->second);
-            // check collision with ships
-            for(iitShip = pdata->ships.begin(); iitShip != pdata->ships.end(); ++iitShip)
+
+        ++itShip;
+        // check collision with ships
+        for(iitShip = pdata->ships.begin(); iitShip != pdata->ships.end(); ++iitShip)
+        {
+            bship = (GOM_Ship *)(iitShip->second);
+            // if we are checking a ship against itself, continue
+            if(ship == bship)
+              continue;
+            if(ship->getHitbox().isCollision(bship->getHitbox()))
             {
-                bship = (GOM_Ship *)(iitShip->second);
-		// if we are checking a ship against itself, continue
-		if(ship == bship)
-		  continue;
-                if(ship->getHitbox().isCollision(bship->getHitbox()))
-                {
-                    // send DELETION msg for both ships to all clients
-                    ostr.clear();
-                    ostr.str("");
-                    ostr << "S" << " " << ship->getObjID() << " 0";
-		    sendMsg.setID(itShip->first);
-                    sendMsg.setAll(ostr.str(), Message::DELETION);
-                    server->write(&sendMsg);
-                    std::cout << "sent deletion: " << sendMsg.getData() << "\n";
-		    
-                    ostr.clear();
-                    ostr.str("");
-                    ostr << "S" << " " << bship->getObjID() << " 0";
-		    sendMsg.setID(iitShip->first);
-                    sendMsg.setAll(ostr.str(), Message::DELETION);
-                    server->write(&sendMsg);
-                    std::cout << "sent deletion: " << sendMsg.getData() << "\n";
+                // send DELETION msg for both ships to all clients
+                ostr.clear();
+                ostr.str("");
+                ostr << "S" << " " << ship->getObjID() << " 0";
+                sendMsg.setID(itShip->first);
+                sendMsg.setAll(ostr.str(), Message::DELETION);
+                server->write(&sendMsg);
+                std::cout << "sent deletion: " << sendMsg.getData() << "\n";
 
-                    // delete the ships
-                    delete pdata->ships[itShip->first];
-		    delete pdata->ships[iitShip->first];
-		    std::cout << "free memory done" << std::endl;
-                    pdata->ships.erase(itShip->first);
-		    pdata->ships.erase(iitShip->first);
-		    std::cout << "map erase done" << std::endl;
+                ostr.clear();
+                ostr.str("");
+                ostr << "S" << " " << bship->getObjID() << " 0";
+                sendMsg.setID(iitShip->first);
+                sendMsg.setAll(ostr.str(), Message::DELETION);
+                server->write(&sendMsg);
+                std::cout << "sent deletion: " << sendMsg.getData() << "\n";
 
-                    break;
-                }
-            } // end of for
+                // delete the ships
+                delete pdata->ships[itShip_copy->first];
+                delete pdata->ships[iitShip->first];
+                std::cout << "free memory done" << std::endl;
+                pdata->ships.erase(itShip_copy->first);
+                pdata->ships.erase(iitShip->first);
+                std::cout << "map erase done" << std::endl;
+
+                break;
+            }
+        } // end of for
     } // end of for
     
     // send UPDATE messages of objects to all clients
